9c9a042eb412cd4aebc509241db6d300fa66711f
✨𝓐𝓾𝓻𝓪𝓬𝓸𝓭𝓮 ✨ <zahiranahirramirez23@gmail.com>
2026-06-30 11:18:22 -0300

    Add delivery zones and update order processing

--- Practica.py
+++ Practica.py
@@ -4,7 +4,21 @@ from datetime import date
 pedidos = {}  # Estructura: {id_pedido: {datos_dict}}
 contador_id_pedido = 1
 historial_clientes = {}  # Estructura: {"NombreCliente": cantidad_compras} 
-fecha_actual = date.today()
+fecha_actual = date.today() #para mostrar la fecha del dia
+#diccionario para las zonas que estan permitidas
+zonas_opciones = {
+    "1": "Resistencia",
+    "2": "Barranqueras",
+    "3": "Fontana",
+    "4": "Puerto Vilelas"
+}
+#diccionario de los precios segun la zona
+tabla_zonas = {
+    "Resistencia": 1000.0,
+    "Barranqueras": 2000.0,
+    "Fontana":3500.0,
+    "Puerto Vilelas":4000.0
+}
 
 def ver_pedidos():
     # Validación por si el diccionario de pedidos está vacío
@@ -15,14 +29,17 @@ def ver_pedidos():
     print("\n  Reporte de operaciones:")
     # Recorremos el diccionario de pedidos usando su clave (id) y valor (datos)
     for id_p, p in pedidos.items():
-        print(f"ID: {id_p} | Cliente: {p['cliente']} | Fecha: {p['fecha']}")
+        fecha_dia = fecha_actual.strftime("%d/%m/%y")
+        print("\nFecha:", fecha_dia) 
+        print(f"ID: {id_p} | Cliente: {p['cliente']} | Fecha: {p['fecha']} | Zona: {p['zona']}")
         print(f"Productos: {p['productos_texto']}")
         print(f"Total: ${p['total']:.2f}")
         print("-" * 30)
 
 def menu_sistema():
     # Se declara global para poder modificar el contador de IDs desde esta función
-    global contador_id_pedido  
+    global contador_id_pedido 
+    global pedidos 
     
     print("\n--- Menu del Delivery ---")
     print("1. Registrar nuevo pedido")
@@ -35,64 +52,70 @@ def menu_sistema():
     if opcion == "1":
         print("\nRegistrando pedido....")
 
-        fecha_dia = fecha_actual.strftime("%d/%m/%y")
-        print("\nFecha:", fecha_dia)
-        print("- - - - - - - - - - - - - - - - - - - - - -")
-        cliente = input("Nombre del cliente: ").strip().capitalize()
-        print("- - - - - - - - - - - - - - - - - - - - - -")
+      
+        cliente = input("\nNombre del cliente: ").strip().capitalize()
         
-        # AGREGADO: Lista para almacenar los productos y variable para acumular el costo total
-        productos_pedidos = []
-        total_pedido = 0.0
+        subtotal=0.0
+        productos_texto=""
         
         while True:
-            nombre_prod = input("\nNombre del producto: ").strip()
+            nombre_prod = input("\nNombre del producto (o '0' para terminar'): ").strip()
+            if nombre_prod.lower() == '0':
+                break
             try:
                 precio_prod = float(input(f"Precio de '{nombre_prod}': $"))
+                subtotal+=precio_prod
                 # Guardamos el producto actual en la lista y sumamos su valor al total
-                productos_pedidos.append({"nombre": nombre_prod, "precio": precio_prod})
-                total_pedido += precio_prod
+                if productos_texto == "":
+                    productos_texto=nombre_prod
+                else:
+                    productos_texto+=","+nombre_prod
+
             except ValueError:
                 print("Error: Precio invalido. Producto no Agregado.")
-                continue
-                
-            print("\n¿Desea seguir agregando productos a su lista?")
-            print("1. Sí, deseo seguir agregando")
-            print("2. No, Finalizar carga de productos")
-            
-            bucle_prod = input("Seleccione una opcion (1 o 2): ").strip()
-            if bucle_prod != "1":
-                break
         
-        # AGREGADO: Guardado efectivo del pedido e incremento del historial
-        if productos_pedidos:
-            # Convertimos la lista de productos en una sola cadena de texto separada por comas
-            nombres_prod_texto = ", ".join([prod["nombre"] for prod in productos_pedidos])
-            
-            # Guardamos toda la información recolectada dentro del diccionario global 'pedidos'
-            pedidos[contador_id_pedido] = {
-                "cliente": cliente,
-                "productos_texto": nombres_prod_texto,
-                "total": total_pedido,
-                "fecha": fecha_dia,
-                "repartidor": "No asignado"
-            }
-            
-            # Registramos la compra en el historial del cliente
-            if cliente in historial_clientes:
-                historial_clientes[cliente] += 1
-            else:
-                historial_clientes[cliente] = 1
-                
-            print(f"\n[Éxito] Pedido #{contador_id_pedido} guardado correctamente.")
-            print(f"Total a pagar: ${total_pedido:.2f}")
-            print(f"Historial: El cliente {cliente} lleva {historial_clientes[cliente]} compra(s) registrada(s).")
-            
-            # Incrementamos el ID para que el siguiente pedido tenga un número único
-            contador_id_pedido += 1
+        if subtotal==0.0:
+            print("operacion cancelada:no se agregaron productos.")
+            return
+
+        print("\nZonas\n1.Resistencia\n2.Barranqueras\n3.Fontana\n4.Puerto Vilelas"),
+        z_op=input("seleccione zona(1-4):").strip()
+
+        if z_op in zonas_opciones:
+            zona_elegida=zonas_opciones[z_op]
         else:
-            print("\n[Cancelado] No se ingresaron productos. Pedido no registrado.")
-            
+            zona_elegida="Resistencia"
+        
+        if cliente not in historial_clientes:
+            historial_clientes[cliente]=0
+        historial_clientes[cliente]+=1
+
+        es_frecuente=(historial_clientes[cliente]==3)
+        costo_envio=0.0 if es_frecuente else tabla_zonas[zona_elegida]
+
+        total_final=subtotal+costo_envio
+        fecha_dia=fecha_actual.strftime("%d/%m/%y")
+        print(f"\nTICKET                                     N°:{contador_id_pedido}")
+        print("Fecha                                  ",fecha_dia) 
+        print("             Sistema Delivery                    ")
+        print("-------------------------------------------------")  
+        print(f"Cliente:{cliente}")
+        print(f"productos:{productos_texto}")
+        print(f"subtotal:${subtotal:.2f}")
+        print(f"Costo del Envío({zona_elegida}):${costo_envio:.2f})")
+        print(f"\nTOTAL A PAGAR:${total_final:.2f}")
+        print("----------------------------------------------------")
+        print("\nDesea seguir con el pago?\n1.Sí\n2.No")
+
+        if input("seleccione (1-2):").strip()=="1":
+            if es_frecuente:
+                historial_clientes[cliente]=0
+
+            print(f"\n¡PEDIDO #{contador_id_pedido} REGISTRADO!")
+            contador_id_pedido+=1
+        else:
+            historial_clientes[cliente]-=1
+              
     elif opcion == "2":
         ver_pedidos()
     elif opcion == "3":
@@ -104,7 +127,7 @@ def menu_sistema():
 
 def ejecutar_inicio():
     while True:
-        print("\n-----Bienvenido a sistema delivery-----")
+        print("\n-----Bienvenido a Sistema Delivery-----")
         print("1. Ver menu")
         print("2. Cerrar sesion")
         op = input("Seleccione una opcion (1 o 2): ").strip()
